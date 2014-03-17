13b32c24d0949ffaf6a1498754f1ae9a73ab8f3a
jonneyfan <jonneyfan@gmail.com>
2014-03-17 17:16:58 +0800

    2014/3/17加入微信

---
+++ nleaps/WeiXin/wx.ashx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace nleaps.WeiXin
+{
+    /// <summary>
+    /// wx 的摘要说明
+    /// </summary>
+    public class wx : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            weixin _wx = new weixin();
+            string postStr = "";
+            if (context.Request.HttpMethod.ToLower() == "post")
+            {
+                Stream s = System.Web.HttpContext.Current.Request.InputStream;
+                byte[] b = new byte[s.Length];
+                s.Read(b, 0, (int)s.Length);
+                postStr = Encoding.UTF8.GetString(b);
+                if (!string.IsNullOrEmpty(postStr)) //请求处理  
+                {
+                    _wx.Handle(postStr);
+                }
+            }
+            else
+            {
+                _wx.Auth();
+            } 
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}

555dc06447de66e31b53aa44059f19384d30e61f
jonneyfan <jonneyfan@gmail.com>
2014-04-18 01:04:24 +0800

    提交试试

---
+++ nleaps/admin/article_new.aspx
@@ -0,0 +1,28 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="article_new.aspx.cs" Inherits="nleaps.admin.article_new" %>
+
+<!DOCTYPE html>
+<html>
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <f:PageManager ID="PageManager1" runat="server" />
+        <f:SimpleForm ID="SimpleForm1" BodyPadding="5px" runat="server" LabelAlign="Top" EnableFrame="true" EnableCollapse="true"
+            Title="表单" Width="850px">
+            <Items>
+                <f:HtmlEditor runat="server" Label="文本编辑器" ID="HtmlEditor1" Height="250px">
+                </f:HtmlEditor>
+                <f:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="获取 HTML 编辑器的内容"
+                    CssClass="inline">
+                </f:Button>
+                <f:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="设置 HTML 编辑器的内容">
+                </f:Button>
+            </Items>
+        </f:SimpleForm>
+    </form>
+</body>
+</html>
+
+</html>
+
---
+++ nleaps/admin/article_new.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Linq;
+using System.Data.Entity;
+using FineUI;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
+namespace nleaps.admin
+{
+    public partial class article_new : PageBase
+    {
+        #region ViewPower
+
+        /// <summary>
+        /// 本页面的浏览权限，空字符串表示本页面不受权限控制
+        /// </summary>
+        public override string ViewPower
+        {
+            get
+            {
+                return "CoreArticleCategoryArticleNew";
+            }
+        }
+
+        #endregion
+
+        #region Page_Load
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+              
+            }
+        }
+
+     
+
+        #endregion
+
+        #region Events
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            HtmlEditor1.Text = "test";
+        }
+        #endregion
+
+
+    }
+}

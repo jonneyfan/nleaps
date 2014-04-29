5aeb50891844513ee5a496a7913d5103fe20da63
jonneyfan <jonneyfan@oulook.com>
2014-04-29 12:04:06 +0800

    4-29添加Models

---
+++ nleaps/Business/Models/WorkFlow/ToDo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+
+
+namespace nleaps.Business.Models.WorkFlow
+{
+    public class ToDo:IKeyID
+    {
+        [Key]
+        public int ID { get; set; }
+        [StringLength(200)]
+        public string WorkName { get; set; }
+        [StringLength(200)]
+        public string FormName { get; set; }
+        public virtual User UserName { get; set; }
+        public DateTime? CreateTime { get; set; }
+        
+        [StringLength(8000)]
+        public string FormContent { get; set; }
+        public virtual WorkFlowWork AttachmentList { get; set; }
+        [StringLength(8000)]
+        public string ReviewOpinion { get; set; }//审评意见
+       
+        
+        [StringLength(50)]
+        public string StateNoe { get; set; }
+        public DateTime? UpdateTime { get; set; }
+        public virtual WorkFlow WorkFlowID { get; set; }
+        public virtual WorkFlowNode NodeID { get; set; }
+        
+       
+        public virtual WorkFlowNode NodeName { get; set; }
+        public virtual User ApproveUserList { get; set; }//审评人列表
+        public virtual User PassUserList { get; set; }//通过人列表
+        
+
+       
+
+    }
+}
--- nleaps/Business/Models/WorkFlow/WorkFlow.cs
+++ nleaps/Business/Models/WorkFlow/WorkFlow.cs
@@ -1,11 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using nleaps.Business.Models.Form;
 
 namespace nleaps.Business.Models.WorkFlow
 {
-    public class WorkFlow
+    public class WorkFlow:IKeyID
     {
+        [Key]
+        public int ID { get;set;}
+        [StringLength(50)]
+        public string WorkFlowName { get; set; }
+        [StringLength(50)]
+        public string FormName { get; set; }
+      
+        [StringLength(2000)]
+        public string Content { get; set; }//简介JianJie
+        
+        public virtual User User{ get ; set;}
+        public virtual FormType FormType { get; set; }
+        public DateTime? CreateTime { get; set; }
+        //public virtual ICollection<User> UserList { get; set; }
     }
 }
